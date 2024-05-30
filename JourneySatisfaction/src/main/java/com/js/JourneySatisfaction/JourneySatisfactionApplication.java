9aa15b71645e2745e373d55a1c6f3868ce613dfe
ram4ads <ram4ads@gmail.com>
2024-05-30 11:07:26 +0530

    connected Local to Github

---
+++ JourneySatisfaction/src/main/java/com/js/JourneySatisfaction/JourneySatisfactionApplication.java
@@ -0,0 +1,13 @@
+package com.js.JourneySatisfaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneySatisfactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneySatisfactionApplication.class, args);
+	}
+
+}
